811c20fb7df4df79a6b9dad0f6f1a0ea61328aa5
IbrMomiji <mominyan@outlook.jp>
2025-06-20 20:05:53 +0900

    inde change

--- index.php
+++ index.php
@@ -3,29 +3,31 @@ require_once __DIR__ . '/system/boot.php';
 $auth = new Auth();
 $initialState = $auth->getInitialState();
 ?>
-<!DOCTYPE html>
-<html lang="ja">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ja" xml:lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>database</title>
-    <link rel="stylesheet" href="style/main.css">
-    <style>
+    <link rel="stylesheet" href="style/main.css" type="text/css" />
+    <style type="text/css">
+    /*<![CDATA[*/
         .icon-notepad { background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="%23000"><path d="M9 1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4l-4-3zm2 9H5v-1h6v1zm0-2H5V7h6v1zm-1-3V2.5L13.5 6H10z"/></svg>'); }
         .icon-file_explorer_dialog { background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="%23000"><path d="M13 2H3v2h10V2zm0 3H3v2h10V5zm0 3H3v2h10V8zM3 11h5v2H3v-2z"/></svg>');}
+    /*]]>*/
     </style>
 </head>
 <body>
-    <!-- Console Window Template -->
+    <!-- コンソールウィンドウのテンプレート -->
     <template id="console-window-template">
         <div class="window-container console-window">
              <div class="resizer top"></div><div class="resizer right"></div><div class="resizer bottom"></div><div class="resizer left"></div><div class="resizer top-left"></div><div class="resizer top-right"></div><div class="resizer bottom-left"></div><div class="resizer bottom-right"></div>
             <div class="title-bar"><div class="title-bar-text"><span class="title-bar-icon icon-console"></span><span class="window-title">database client</span></div><div class="window-controls"><span class="minimize-btn">_</span><span class="maximize-btn">&#10065;</span><span class="close-btn">X</span></div></div>
-            <div class="console-body"><div class="console-output"></div><div class="input-line"><span class="prompt"></span><input type="text" class="console-input" spellcheck="false" autocomplete="off"></div></div>
+            <div class="console-body"><div class="console-output"></div><div class="input-line"><span class="prompt"></span><input type="text" class="console-input" spellcheck="false" autocomplete="off" /></div></div>
         </div>
     </template>
     
-    <!-- Explorer Window Template -->
+    <!-- エクスプローラーウィンドウのテンプレート -->
     <template id="explorer-window-template">
         <div class="window-container explorer-window">
             <div class="resizer top"></div><div class="resizer right"></div><div class="resizer bottom"></div><div class="resizer left"></div><div class="resizer top-left"></div><div class="resizer top-right"></div><div class="resizer bottom-left"></div><div class="resizer bottom-right"></div>
@@ -34,7 +36,7 @@ $initialState = $auth->getInitialState();
         </div>
     </template>
 
-    <!-- Notepad Window Template -->
+    <!-- メモ帳ウィンドウのテンプレート -->
     <template id="notepad-window-template">
         <div class="window-container notepad-window">
             <div class="resizer top"></div><div class="resizer right"></div><div class="resizer bottom"></div><div class="resizer left"></div><div class="resizer top-left"></div><div class="resizer top-right"></div><div class="resizer bottom-left"></div><div class="resizer bottom-right"></div>
@@ -43,7 +45,7 @@ $initialState = $auth->getInitialState();
         </div>
     </template>
 
-    <!-- File Explorer Dialog Window Template -->
+    <!-- ファイルエクスプローラーダイアログウィンドウのテンプレート -->
     <template id="file_explorer_dialog-window-template">
         <div class="window-container file-explorer-dialog-window">
             <div class="resizer top"></div><div class="resizer right"></div><div class="resizer bottom"></div><div class="resizer left"></div><div class="resizer top-left"></div><div class="resizer top-right"></div><div class="resizer bottom-left"></div><div class="resizer bottom-right"></div>
@@ -56,7 +58,11 @@ $initialState = $auth->getInitialState();
     <div class="snap-indicator" style="display: none;"></div>
     <div id="context-menu-container"></div>
 
-    <script> const initialClientState = <?php echo json_encode($initialState); ?>; </script>
+    <script type="text/javascript">
+    //<![CDATA[
+        const initialClientState = <?php echo json_encode($initialState); ?>;
+    //]]>
+    </script>
     <script src="js/api.js" type="module"></script>
     <script src="js/contextMenu.js" type="module"></script>
     <script src="js/windowManager.js" type="module"></script>
